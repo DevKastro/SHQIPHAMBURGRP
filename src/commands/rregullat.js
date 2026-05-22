6983a7b151e90c18a51eaadd4988bc42dd2c996a
DevKastro <visi.senja11@gmail.com>
2026-05-22 20:09:46 +0200

    Update rregullat.js

--- src/commands/rregullat.js
+++ src/commands/rregullat.js
@@ -22,7 +22,7 @@ export default {
 **6. 🎭 Vlerësoni Jetën (Value Your Life):** Binduni kur ju drejtohet arma, mos bëni si superhero.
             `)
             // KETU VENDOS LINKUN E FOTOS QE DESHIRON TE SHFAQET POSHTE TEKSTIT (FIKS SI NE FOTO)
-            .setImage('https://cdn.discordapp.com/attachments/1330626767844540518/1504956513893093466/IMG_6063.png?ex=6a117139&is=6a101fb9&hm=9767756cc5eddaf34f63b2590d2aca94c4fbc7298dea718cf4d374cf10c11321&'); 
+            .setImage('https://cdn.discordapp.com/attachments/1350496684634804294/1507445149817372672/IMG_6063.png?ex=6a11ed32&is=6a109bb2&hm=afaa366db7d542f5551658e1ac33754992f7d05cd3cc9c755fd64791b2b77f02&'); 
 
         // Boti i pergjigjet lojtarit duke derguar kutine e dizajnuar
         await interaction.reply({ embeds: [rulesEmbed] });
