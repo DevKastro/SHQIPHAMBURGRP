a6f908b356a92d70e83382b35c59588f0c9a7827
DevKastro <visi.senja11@gmail.com>
2026-05-22 20:45:37 +0200

    Update rregullat.js

--- src/commands/rregullat.js
+++ src/commands/rregullat.js
@@ -14,12 +14,80 @@ export default {
                 iconURL: 'https://imgur.com' // Vendos linkun e logos tende nese deshiron
             })
             .setDescription(`
-**1. 🚫 RDM (Random Deathmatch):** Ndalohet vrasja e lojtarëve pa arsye në lojë.
-**2. 🚗 VDM (Vehicle Deathmatch):** Ndalohet përdorimi i makinave si armë për të shtypur lojtarët.
-**3. 🧠 Metagaming:** Ndalohet përdorimi i informacioneve jashtë loje brenda në lojë.
-**4. 💪 Powergaming:** Ndalohet kryerja e veprimeve jorealiste apo mbinatyrore.
-**5. 🛑 Combat Logging:** Ndalohet dalja nga loja (Alt+F4) në mes të aksionit aktiv.
-**6. 🎭 Vlerësoni Jetën (Value Your Life):** Binduni kur ju drejtohet arma, mos bëni si superhero.
+
+# 📜 RREGULLAT E ROLEPLAY NË EMERGENCY HAMBURG
+
+
+# 1. ❌ RDM – Random Deathmatch
+
+# Vrasje pa asnjë arsye RP, thjesht e sheh dikë dhe e vret.
+➡️ (3 ditë ban)
+
+# 2. ❌ VDM – Vehicle Deathmatch
+
+# Shtypje lojtarësh me makinë pa roleplay ose për qejf.
+➡️ (2 ditë ban)
+
+# 3. ❌ Metagaming
+
+# Përdor informacion nga jashtë loje (si Discord apo stream) për të përfituar në lojë.
+➡️ (2 ditë ban)
+
+# 4. ❌  Powergaming
+
+# Veprime të pamundura si në film (shembull: arratisesh me duar të lidhura ose lufton 4 veta vetëm).
+➡️ (1 ditë ban)
+
+# 5. ❌  Fail RP
+
+# Thyerje e logjikës RP – p.sh. vdes dhe flet, ose s'bën RP kur plagosesh.
+➡️ (1 ditë ban)
+
+# 6.  ❌ Fear RP
+
+#  Nuk ndjen “frikë” kur kërcënohesh – p.sh. je nën armë dhe vrapon si hero.
+➡️ (1 ditë ban)
+
+# 7. ❌  Combat Logging
+
+# Del nga serveri që të shpëtosh kur je arrestuar, plagosur apo në RP.
+➡️ (3 ditë ban)
+
+# 8. ❌ Trolling / No RP Intent
+
+# Futesh në lojë vetëm për të prishur RP-në e të tjerëve ose nuk bën fare RP.
+➡️ (3-7 ditë ban)
+
+# 9. ❌ Cop Baiting
+
+# Provokon kot policinë vetëm për aksion – pa asnjë arsye RP.
+➡️ (1 ditë ban)
+
+# 10. ❌ Fyerje / Racizëm / Ofendime OOC
+
+# Flet jashtë RP duke ofenduar ose përdor fjalor të pistë / racist.
+➡️ (3-7 ditë ban) ose (ban permanent)
+
+# 11. ❌ Zbulim info jashtë RP
+
+# Sheh dikë me armë dhe thua “ka armë” pa qenë në RP ose pa e parë në lojë.
+➡️ (1 ditë ban)
+
+# 12. Mosbindje ndaj Staff-it / Mashtim
+
+# Gënjen ose nuk dëgjon adminët.
+➡️ (2 ditë ban)
+
+# 13. ❌ Bug Abuse / Exploits
+
+# Përdor gabime të lojës për përfitim.
+➡️ (ban 4)
+
+# ⚠️   Kujdes
+
+# Pas 2 paralajmërimeve, shkelja e tretë mer ban direkt.
+
+# Nëse ke një arsye, shpjegoje me respekt te staff-i ose në Discord tickets.
             `)
             // KETU VENDOS LINKUN E FOTOS QE DESHIRON TE SHFAQET POSHTE TEKSTIT (FIKS SI NE FOTO)
             .setImage('https://cdn.discordapp.com/attachments/1505541133458083910/1507449358444921013/Screenshot_2026-05-22_201931.PNG?ex=6a11f11e&is=6a109f9e&hm=86959afc6f20baf2661ae64debf927d5f136610eb0c79252e5b811fe99f3a470&'); 
