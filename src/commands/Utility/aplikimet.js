f4a5e6385426c52cabdad6d33fbe00067d4c8bea
DevKastro <visi.senja11@gmail.com>
2026-05-27 22:12:35 +0200

    Update aplikimet.js

--- src/commands/Utility/aplikimet.js
+++ src/commands/Utility/aplikimet.js
@@ -6,7 +6,8 @@ export default {
     .setDescription('Shfaq njoftimin dhe linkun e aplikimeve për Polici'),
 
   async execute(interaction) {
-    const linkAplikimi = 'https://forms.gle'; 
+    // Linku i saktë publik për lojtarët
+    const linkAplikimi = 'https://docs.google.com/forms/d/1O_HjS9_wb9DJSnVI3D9sxgoCn1Inb8F4RRoHJ4U42rI/edit'; 
 
     const embed = new EmbedBuilder()
       .setColor('#0000ff') // Ngjyra Blu e Policisë
@@ -21,7 +22,6 @@ export default {
         `🔗 [** Kliko Këtu Për Të Apliku Për Polici **](${linkAplikimi})\n\n` +
         `ℹ️ * ** Shënim i rëndësishëm:* Nëse jeni pranuar, ju do të merrni linkun e sektorit të policisë direkt në DM (Mesazh Privat)**.`
       )
-      // Këtu vendoset fotoja jote zyrtare e qytetit në fund të njoftimit
       .setImage('https://cdn.discordapp.com/attachments/1505541145311051817/1509262679062220810/1779018008984.png?ex=6a1889e7&is=6a173867&hm=9d06bfc8afd060ded438a7d333f9277d37e4bdbaf9dffd2b7465471670adfb5d&')
       .setTimestamp();
 
