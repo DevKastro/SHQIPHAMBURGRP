266590dcb5e0c8a0f21981589af1d448b7000c54
DevKastro <visi.senja11@gmail.com>
2026-05-27 21:51:23 +0200

    Update aplikimet.js

--- src/commands/Utility/aplikimet.js
+++ src/commands/Utility/aplikimet.js
@@ -23,3 +23,8 @@ export default {
       )
       // Këtu vendoset fotoja jote zyrtare e qytetit në fund të njoftimit
       .setImage('https://cdn.discordapp.com/attachments/1505541145311051817/1509262679062220810/1779018008984.png?ex=6a1889e7&is=6a173867&hm=9d06bfc8afd060ded438a7d333f9277d37e4bdbaf9dffd2b7465471670adfb5d&')
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
